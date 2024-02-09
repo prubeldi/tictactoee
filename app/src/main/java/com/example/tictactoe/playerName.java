f4295eb3fa08abfb9950b440f4788d9078951b39
feihong-guapo <98840820+feihong-guapo@users.noreply.github.com>
2024-02-09 19:20:11 +0100

    hola buenos dias

--- app/src/main/java/com/example/tictactoe/playerName.java
+++ app/src/main/java/com/example/tictactoe/playerName.java
@@ -1,4 +1,19 @@
 package com.example.tictactoe;
 
-public class playerName {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+public class playerName extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText  playNameEt  = findViewById(R.id.playerNameEt);
+        final AppCompatButton startGameBtn = findViewById(R.id.startGameBtn);
+
+    }
 }
