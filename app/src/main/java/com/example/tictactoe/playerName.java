cb8290138e3947fd6e8914fa429022af1bc809a1
feihong-guapo <98840820+feihong-guapo@users.noreply.github.com>
2024-02-13 16:38:56 +0100

    hola buenos dias

--- app/src/main/java/com/example/tictactoe/playerName.java
+++ app/src/main/java/com/example/tictactoe/playerName.java
@@ -1,7 +1,10 @@
 package com.example.tictactoe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -14,6 +17,19 @@ public class playerName extends AppCompatActivity {
 
         final EditText  playNameEt  = findViewById(R.id.playerNameEt);
         final AppCompatButton startGameBtn = findViewById(R.id.startGameBtn);
-
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String getplayerName = playNameEt.getText().toString();
+                if (getplayerName.isEmpty()){
+                    Toast.makeText(playerName.this, "", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent intent = new Intent(playerName.this,MainActivity.class);
+                    intent.putExtra("playerName",getplayerName);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
